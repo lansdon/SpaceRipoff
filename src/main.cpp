10ce3570fecbbd63be1592b52be034f1321560ef
Lansdon <lobotomizer2000@yahoo.com>
2012-12-15 03:16:29 -0800

    Initial Commit    - SFML Libraries linked and lib + include files added to project    - Chipmunk Libraries linked and lib + include files added to project    - Added setup file for MSVS

---
+++ src/main.cpp
@@ -0,0 +1,25 @@
+/*
+	Space Ripoff - 2D Space Game
+	By: Lansdon Page, Steven Ahl
+	Started: 12/15/2012
+
+
+*/
+
+
+
+
+#include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
+#include <chipmunk/chipmunk.h>
+#include <iostream>
+
+
+int main(int argc, int argv[]) {
+
+	std::cout << "SPACE RIPOFF GAME OF EPIC PROPORTIONS!!\n";
+
+
+	std::cin.get();
+
+}
