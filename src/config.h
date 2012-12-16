85db4af25fbb8cd467983000d480d0c69e09286a
Lansdon Page <lansdon.page@email.wsu.edu>
2012-12-16 02:42:07 -0800

    - moved pong sample code to seperate file - new config.h file for program settings - Started basic framework/loop for the game - added space background

---
+++ src/config.h
@@ -0,0 +1,20 @@
+/*
+	Configuration settings
+
+
+*/
+
+#define TITLE "Space Ripoff"
+
+// Initial resolution 
+#define START_RESOLUTION_X 800
+#define START_RESOLUTION_Y 600
+#define START_PIXEL_DEPTH 32
+
+
+#define WORLD_DIMENSION_X = 1000 // kilometers
+#define WORLD_DIMENSION_Y = 1000 // kilometers
+
+#define GRAVITY 9.8
+const float PI = 3.14159265359;
+
